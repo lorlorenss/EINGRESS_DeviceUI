811f2703f36d0efc789c7681378b107562ee2584
laurence añasco <lanasco@jairosoftcom.onmicrosoft.com>
2024-07-04 08:36:17 +0800

    Changes for registration component instructions

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-registration',
@@ -6,13 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+ @ViewChild('inputElement', { static: true }) inputElement!: ElementRef;
   isHidden: boolean = false;
+  instruction: string = 'Instructions';
 
-  submitData(){
+  constructor(private router: Router) {
+    // Focus on the input textbox when the component is initialized
+    setTimeout(() => {
+      this.inputElement.nativeElement.focus();
+    });
+  }
 
-}
+  @HostListener('document:click', ['$event'])
+
+  onClick(event: MouseEvent) {
+    // Focus on the input textbox whenever a click event occurs on the document
+    this.inputElement.nativeElement.focus();
+  
+    // Prevent the default behavior of the click event to ensure the input textbox remains focused
+    event.preventDefault();
+  }
+  // onInput(event: Event): void {
+  //   // Log the typed input to the console
+  //   console.log('Typed input:', (event.target as HTMLInputElement).value);
+  // }
 
 onFocus(): void{
   this.isHidden = false;
 }
+
+// onBlur(): void{
+// setTimeout(() => {
+//   this.isHidden = false;
+// })
+// }
+
+
+submitData(): void {
+  // Perform data submission logic here
+  this.instruction = this.inputElement.nativeElement.value;
+
+  if (this.instruction.includes("ID:")) {
+    setTimeout(() => {
+      this.router.navigateByUrl('landingPage');
+    }, 5000); // 5000 milliseconds = 5 seconds
+  }
+  this.inputElement.nativeElement.value = '';
+  this.inputElement.nativeElement.focus();
+}
+
 }
