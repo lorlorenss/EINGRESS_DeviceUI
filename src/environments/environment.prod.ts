805b6b77bd8095255e0dc89b6ce52af9fde5d49a
laurence añasco <lanasco@jairosoftcom.onmicrosoft.com>
2024-07-16 10:45:43 +0800

    Added ips for environment prod in baseUrls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production : true,
+    // baseURL : 'http://192.168.42.64:4200/' 
+    // baseURL : 'http://192.168.42.68:4200/' 
     baseURL : 'http://localhost:4200/' 
 }
