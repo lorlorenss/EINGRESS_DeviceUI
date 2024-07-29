3d611970151bcd789d4649888a932fcb9504582f
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-07-29 10:27:35 +0800

    port 3000

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production : true,
    
     // baseURL : 'http://localhost:4200/' 
-        baseURL : 'http://localhost:3100/' 
+        baseURL : 'http://localhost:3000/' 
 }
