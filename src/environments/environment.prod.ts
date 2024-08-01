145e85d7cc8fd7c98097feb625ac27c1596b75ec
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-08-01 11:18:07 +0800

    updated jairo api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
     production : true,
    
     // api url
-        baseURL : 'http://104.43.100.222:3000/' 
-}
+        // baseURL : 'http://104.43.100.222:3000/' 
+        //apiurl from jairo api
+        baseURL : 'https://jairoeingressapi-bzaphjgjeze6bgh2.southeastasia-01.azurewebsites.net/' 
+    }
